3ecb7d30b9c89fc613c0ae340287f89576bdf359
Máximo Cuadros <mcuadros@gmail.com>
2017-03-06 18:48:41 +0100

    build: fix require bblfsh-tools inside of a container

--- assets/build/bindata.go
+++ assets/build/bindata.go
@@ -7,6 +7,7 @@
 // etc/build/make/bootstrap.mk
 // etc/build/make/functions.mk
 // etc/build/make/help.mk
+// etc/build/make/manifest.mk
 // etc/build/make/rules.mk
 // DO NOT EDIT!
 
@@ -64,16 +65,11 @@ tmplocation = $(sdklocation)/tmp
 $(shell mkdir -p $(tmplocation))
 
 bblfsh-sdk := $(shell command -v bblfsh-sdk 2> /dev/null)
-ifeq ($(strip $(bblfsh-sdk)),)
-    $(error `+"`"+`bblfsh-sdk`+"`"+` should be available from the PATH)
-endif
-
 bblfsh-tools := $(shell command -v bblfsh-tools 2> /dev/null)
-ifeq ($(strip $(bblfsh-tools)),)
-    $(error `+"`"+`bblfsh-tools`+"`"+` should be available from the PATH)
+ifdef bblfsh-tools
+    $(shell $(bblfsh-tools) manifest > $(manifest))
 endif
 
-$(shell $(bblfsh-tools) manifest > $(manifest))
 include $(sdklocation)/make/manifest.mk
 include $(sdklocation)/make/functions.mk
 include $(sdklocation)/make/bootstrap.mk
@@ -91,7 +87,7 @@ func makefile() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "Makefile", size: 1105, mode: os.FileMode(420), modTime: time.Unix(1, 0)}
+	info := bindataFileInfo{name: "Makefile", size: 935, mode: os.FileMode(420), modTime: time.Unix(1, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -345,6 +341,27 @@ func makeHelpMk() (*asset, error) {
 	return a, nil
 }
 
+var _makeManifestMk = []byte(`LANGUAGE=rust
+RUNTIME_OS=debian
+RUNTIME_NATIVE_VERSION=nightly-2017-02-20
+RUNTIME_GO_VERSION=1.8
+`)
+
+func makeManifestMkBytes() ([]byte, error) {
+	return _makeManifestMk, nil
+}
+
+func makeManifestMk() (*asset, error) {
+	bytes, err := makeManifestMkBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "make/manifest.mk", size: 97, mode: os.FileMode(420), modTime: time.Unix(1, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
 var _makeRulesMk = []byte(`BUILD_ID := $(shell date +"%m-%d-%Y_%H_%M_%S")
 BUILD_PATH := $(location)/build
 
@@ -528,6 +545,7 @@ var _bindata = map[string]func() (*asset, error){
 	"make/bootstrap.mk": makeBootstrapMk,
 	"make/functions.mk": makeFunctionsMk,
 	"make/help.mk": makeHelpMk,
+	"make/manifest.mk": makeManifestMk,
 	"make/rules.mk": makeRulesMk,
 }
 
@@ -583,6 +601,7 @@ var _bintree = &bintree{nil, map[string]*bintree{
 		"bootstrap.mk": &bintree{makeBootstrapMk, map[string]*bintree{}},
 		"functions.mk": &bintree{makeFunctionsMk, map[string]*bintree{}},
 		"help.mk": &bintree{makeHelpMk, map[string]*bintree{}},
+		"manifest.mk": &bintree{makeManifestMk, map[string]*bintree{}},
 		"rules.mk": &bintree{makeRulesMk, map[string]*bintree{}},
 	}},
 }}
