64a34980615dfcf2e992ca66891342579e0d3126
Juanjo Alvarez <juanjux@gmail.com>
2017-05-30 12:46:40 +0200

    Added disclaimer about the boolean operator counting

--- uast/utils.go
+++ uast/utils.go
@@ -32,16 +32,26 @@ func Tokens(n *Node) []string {
 // If the children contain more than one function definitions, the value will not be averaged between
 // the total number of function declarations but given as a total.
 //
-//
 // Some practical implementations counting tokens in the code. They sometimes differ; for example
 // some of them count the switch "default" as an incrementor, some consider all return values minus the
 // last, some of them consider "else" (which is wrong IMHO, but not for elifs, remember than the IfElse
 // token in the UAST is really an Else not an "else if", elseifs would have a children If token), some
 // consider throw and finally while others only the catch, etc.
 //
+// Examples:
 // GMetrics: http://gmetrics.sourceforge.net/gmetrics-CyclomaticComplexityMetric.html
 // Go: https://github.com/fzipp/gocyclo/blob/master/gocyclo.go#L214
 // SonarQube (include rules for many languages): https://docs.sonarqube.org/display/SONAR/Metrics+-+Complexity
+//
+// IMPORTANT DISCLAIMER: McCabe definition specifies clearly that boolean operations should increment the
+// count in 1 for every boolean element when the language if the language evaluates conditions in
+// short-circuit.  Unfortunately in the current version of the UAST we don't specify these language invariants
+// and also we still haven't defined how we are going to represent the boolean expressions (which also would
+// need a tree transformation process in the pipeline that we lack) so lacking a better way of detecting for
+// all  languages what symbols or literals are part of a boolean expression we count the boolean operators
+// themselves which should work for short-circuit infix languages but not prefix or infix languages that can
+// evaluate more than two items with a single operator.  (FIXME when both things are solved in the UAST
+// definition and the SDK).
 func CyclomaticComplexity(n *Node)  int {
 	complx := 1
 
