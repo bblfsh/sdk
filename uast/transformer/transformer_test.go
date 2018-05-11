6295c4a5630995014540dbad4ab1094ffe18a544
Denys Smirnov <denys@sourced.tech>
2018-05-11 04:25:14 +0300

    transformer: add more test cases
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- uast/transformer/transformer_test.go
+++ uast/transformer/transformer_test.go
@@ -14,6 +14,47 @@ var mappingCases = []struct {
 	m        Transformer
 }{
 	{
+		name: "trim meta",
+		inp: u.Object{
+			"the_root": u.Object{
+				"k": u.String("v"),
+			},
+		},
+		m: ResponseMetadata{
+			TopLevelIsRootNode: false,
+		},
+		exp: u.Object{
+			"k": u.String("v"),
+		},
+	},
+	{
+		name: "leave meta",
+		inp: u.Object{
+			"the_root": u.Object{
+				"k": u.String("v"),
+			},
+		},
+		m: ResponseMetadata{
+			TopLevelIsRootNode: true,
+		},
+	},
+	{
+		name: "roles dedup",
+		inp: u.Array{
+			u.Object{
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(1, 2, 1),
+			},
+		},
+		m: RolesDedup(),
+		exp: u.Array{
+			u.Object{
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(1, 2),
+			},
+		},
+	},
+	{
 		name: "typed and generic",
 		inp: u.Array{
 			u.Object{
@@ -42,13 +83,14 @@ var mappingCases = []struct {
 				"k": Var("x"),
 			}, Obj{
 				"key": Var("x"),
-			}),
+			}, 10),
 		),
 		exp: u.Array{
 			u.Object{
-				u.KeyType: u.String("typed"),
-				"p":       u.String("val1"),
-				"key":     u.String("v"),
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(10),
+				"p":        u.String("val1"),
+				"key":      u.String("v"),
 			},
 			u.Object{
 				"p": u.String("val2"),
@@ -56,6 +98,33 @@ var mappingCases = []struct {
 			},
 			u.Object{
 				"pred2": u.String("val3"),
+			},
+		},
+	},
+	{
+		name: "annotate no roles",
+		inp: u.Array{
+			u.Object{
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(1),
+				"pred":     u.String("val1"),
+			},
+			u.Object{
+				u.KeyType: u.String("typed"),
+				"pred":    u.String("val2"),
+			},
+		},
+		m: AnnotateIfNoRoles("typed", 10),
+		exp: u.Array{
+			u.Object{
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(1),
+				"pred":     u.String("val1"),
+			},
+			u.Object{
+				u.KeyType:  u.String("typed"),
+				u.KeyRoles: u.RoleList(10),
+				"pred":     u.String("val2"),
 			},
 		},
 	},
