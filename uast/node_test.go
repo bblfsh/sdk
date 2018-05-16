e190bec6195340d21534491dc0c7bd5c1cd1419c
Denys Smirnov <denys@sourced.tech>
2018-05-16 19:37:10 +0300

    uast: add tests for node comparison
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- uast/node_test.go
+++ uast/node_test.go
@@ -98,3 +98,150 @@ func TestApply(t *testing.T) {
 		},
 	}, out)
 }
+
+var casesEqual = []struct {
+	name   string
+	n1, n2 Node
+	exp    bool
+}{
+	{
+		name: "nil object vs empty object",
+		n1:   Object{}, n2: (Object)(nil),
+		exp: true,
+	},
+	{
+		name: "nil array vs empty array",
+		n1:   Array{}, n2: (Array)(nil),
+		exp: true,
+	},
+	{
+		name: "nil vs nil object",
+		n1:   nil, n2: (Object)(nil),
+		exp: false,
+	},
+	{
+		name: "nil vs nil array",
+		n1:   nil, n2: (Array)(nil),
+		exp: false,
+	},
+	{
+		name: "nil vs empty object",
+		n1:   nil, n2: Object{},
+		exp: false,
+	},
+	{
+		name: "nil vs empty array",
+		n1:   nil, n2: Array{},
+		exp: false,
+	},
+	{
+		name: "int vs float",
+		n1:   Int(0), n2: Float(0),
+		exp: false,
+	},
+	{
+		name: "nested object",
+		n1: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				Object{"k4": Bool(false)},
+				nil,
+				Int(1),
+			},
+			"k3": nil,
+		},
+		exp: true,
+	},
+	{
+		name: "nested object new field",
+		n1: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				Object{"k4": Bool(false)},
+				nil,
+				Int(1),
+			},
+		},
+		n2: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				Object{"k4": Bool(false), "k5": nil},
+				nil,
+				Int(1),
+			},
+		},
+		exp: false,
+	},
+	{
+		name: "nested object nil field",
+		n1: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				Int(1),
+			},
+		},
+		n2: Object{
+			"k1": nil,
+			"k2": Array{
+				Int(1),
+			},
+		},
+		exp: false,
+	},
+	{
+		name: "nested array wrong length",
+		n1: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				nil,
+				Int(1),
+			},
+		},
+		n2: Object{
+			"k1": String("v1"),
+			"k2": Array{
+				nil,
+				Int(1),
+				nil,
+			},
+		},
+		exp: false,
+	},
+	{
+		name: "object wrong type",
+		n1:   Object{}, n2: String(""),
+		exp: false,
+	},
+	{
+		name: "array wrong type",
+		n1:   Array{}, n2: String(""),
+		exp: false,
+	},
+	{
+		name: "string wrong type",
+		n1:   String(""), n2: Int(0),
+		exp: false,
+	},
+	{
+		name: "int wrong type",
+		n1:   Int(0), n2: String(""),
+		exp: false,
+	},
+	{
+		name: "int wrong type",
+		n1:   Bool(false), n2: String(""),
+		exp: false,
+	},
+}
+
+func TestNodeEqual(t *testing.T) {
+	for _, c := range casesEqual {
+		t.Run(c.name, func(t *testing.T) {
+			n1, n2 := c.n1, c.n2
+			if n2 == nil {
+				n2 = n1
+			}
+			require.Equal(t, c.exp, Equal(n1, n2))
+		})
+	}
+}
