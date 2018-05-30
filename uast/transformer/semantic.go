fb7bd14905db648563e1302bf1e994eacfdf6dfe
Denys Smirnov <denys@sourced.tech>
2018-05-30 14:29:46 +0300

    transformer: add comment helpers for semantic UAST
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- uast/transformer/semantic.go
+++ uast/transformer/semantic.go
@@ -3,8 +3,10 @@ package transformer
 import (
 	"fmt"
 	"strings"
+	"unicode"
 
 	"gopkg.in/bblfsh/sdk.v2/uast"
+	"gopkg.in/bblfsh/sdk.v2/uast/nodes"
 )
 
 func UASTType(uobj interface{}, op ObjectOp) ObjectOp {
@@ -44,4 +46,91 @@ func MapSemantic(name, nativeType string, semType interface{}, pos map[string]st
 	so.SetField(uast.KeyPos, sp)
 	do.SetField(uast.KeyPos, dp)
 	return Map(name, so, UASTType(semType, do))
+}
+
+func CommentText(tokens [2]string, vr string) Op {
+	return commentUAST{
+		tokens: tokens,
+		text:   vr + "_text", pref: vr + "_pref", suff: vr + "_suff", tab: vr + "_tab",
+	}
+}
+
+func CommentNode(block bool, vr string, pos Op) ObjectOp {
+	obj := Obj{
+		"Block":  Bool(block),
+		"Text":   Var(vr + "_text"),
+		"Prefix": Var(vr + "_pref"),
+		"Suffix": Var(vr + "_suff"),
+		"Tab":    Var(vr + "_tab"),
+	}
+	if pos != nil {
+		obj[uast.KeyPos] = pos
+	}
+	return UASTType(uast.Comment{}, obj)
+}
+
+type commentUAST struct {
+	tokens          [2]string
+	text            string
+	pref, suff, tab string
+}
+
+func (op commentUAST) Check(st *State, n nodes.Node) (bool, error) {
+	s, ok := n.(nodes.String)
+	if !ok {
+		return false, nil
+	}
+	text := string(s)
+	if !strings.HasPrefix(text, op.tokens[0]) || !strings.HasSuffix(text, op.tokens[1]) {
+		return false, nil
+	}
+	text = strings.TrimPrefix(text, op.tokens[0])
+	text = strings.TrimSuffix(text, op.tokens[1])
+	var (
+		pref, suff, tab string
+	)
+
+	// find prefix
+	i := 0
+	for ; i < len(text); i++ {
+		if r := rune(text[i]); unicode.IsLetter(r) || unicode.IsNumber(r) {
+			break
+		}
+	}
+	pref = text[:i]
+	text = text[i:]
+
+	// find suffix
+	i = len(text) - 1
+	for ; i >= 0 && unicode.IsSpace(rune(text[i])); i-- {
+	}
+	suff = text[i+1:]
+	text = text[:i+1]
+
+	// TODO: set tab
+
+	err := st.SetVars(Vars{
+		op.text: nodes.String(text),
+		op.pref: nodes.String(pref),
+		op.suff: nodes.String(suff),
+		op.tab:  nodes.String(tab),
+	})
+	return err == nil, err
+}
+
+func (op commentUAST) Construct(st *State, n nodes.Node) (nodes.Node, error) {
+	var (
+		text, pref, suff, tab nodes.String
+	)
+
+	err := st.MustGetVars(VarsPtrs{
+		op.text: &text,
+		op.pref: &pref, op.suff: &suff, op.tab: &tab,
+	})
+	if err != nil {
+		return nil, err
+	}
+	// FIXME: handle tab
+	text = pref + text + suff
+	return nodes.String(op.tokens[0] + string(text) + op.tokens[1]), nil
 }
