a4e5194367fffe36b80e233dbcfd9dfb809da048
Denys Smirnov <denys@sourced.tech>
2018-07-20 19:11:51 +0300

    build: allow docker push on vX branches, but don't push latest
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- build/push.go
+++ build/push.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 )
 
-func PushEnabled() bool {
-	return ciBranch() == "master" && ciTag() != "" && !ciIsPR()
+var reVers = regexp.MustCompile(`^v\d+`)
+
+func pushEnabled() bool {
+	return ciTag() != "" && !ciIsPR()
+}
+
+func pushTagEnabled() bool {
+	return pushEnabled() && reVers.MatchString(ciBranch())
+}
+
+func pushLatestEnabled() bool {
+	return ciBranch() == "master" && pushEnabled()
 }
 
 func (d *Driver) Push(image string) error {
-	if !PushEnabled() {
+	if !pushTagEnabled() && !pushLatestEnabled() {
 		return fmt.Errorf("push disabled")
 	} else if image == "" {
 		return fmt.Errorf("image should be specified")
@@ -24,30 +35,31 @@ func (d *Driver) Push(image string) error {
 	if err != nil {
 		return err
 	}
-	imageName := "bblfsh/" + m.Language + "-driver"
-	latestImage := imageName + ":latest"
-	versImage := imageName + ":" + tag
 	if user := os.Getenv("DOCKER_USERNAME"); user != "" {
 		cmd := exec.Command("docker", "login", "-u="+user, "-p="+os.Getenv("DOCKER_PASSWORD"))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
 			return err
 		}
 	}
-	err = execIn("", nil, "docker", "tag", image, versImage)
-	if err != nil {
-		return err
+	push := func(id, name string) error {
+		err = execIn("", nil, "docker", "tag", id, name)
+		if err != nil {
+			return err
+		}
+		return execIn("", nil, "docker", "push", name)
 	}
-	err = execIn("", nil, "docker", "tag", versImage, latestImage)
-	if err != nil {
-		return err
+	imageName := "bblfsh/" + m.Language + "-driver"
+	if pushTagEnabled() {
+		if err := push(image, imageName+":"+tag); err != nil {
+			return err
+		}
 	}
-	err = execIn("", nil, "docker", "push", versImage)
-	if err != nil {
-		return err
-	}
-	err = execIn("", nil, "docker", "push", latestImage)
-	if err != nil {
-		return err
+	if pushLatestEnabled() {
+		if err := push(image, imageName+":latest"); err != nil {
+			return err
+		}
 	}
 	return nil
 }
