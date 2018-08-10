05264ca7db949c89ba9f2c9fec4996cc4aad98ad
Denys Smirnov <denys@sourced.tech>
2018-08-10 12:03:09 +0300

    handle the case when only Gopkg.toml exists
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- build/golang.go
+++ build/golang.go
@@ -13,6 +13,14 @@ func (d *Driver) depEnsure() error {
 	} else if err != nil {
 		return err
 	}
+	if _, err := os.Stat(d.path("Gopkg.lock")); os.IsNotExist(err) {
+		err = execIn(d.root, nil, "dep", "ensure")
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
 	if _, err := os.Stat(d.path("vendor")); os.IsNotExist(err) {
 		err = execIn(d.root, nil, "dep", "ensure", "--vendor-only")
 		if err != nil {
