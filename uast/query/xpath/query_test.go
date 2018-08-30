908ab69fdfce3f532fcc81c84c33e10a48c6a9d5
Denys Smirnov <denys@sourced.tech>
2018-08-30 20:24:55 +0300

    xpath: rewrite projection code to follow v2 spec
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- uast/query/xpath/query_test.go
+++ uast/query/xpath/query_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
 	"gopkg.in/bblfsh/sdk.v2/uast"
 	"gopkg.in/bblfsh/sdk.v2/uast/nodes"
 	"gopkg.in/bblfsh/sdk.v2/uast/query"
 )
 
-func toNode(o interface{}) nodes.Node {
+func mustNode(o interface{}) nodes.Node {
 	n, err := uast.ToNode(o)
 	if err != nil {
 		panic(err)
@@ -19,17 +20,137 @@ func toNode(o interface{}) nodes.Node {
 
 func TestFilter(t *testing.T) {
 	var root = nodes.Array{
-		toNode(uast.Identifier{Name: "Foo"}),
+		mustNode(uast.Identifier{Name: "Foo"}),
 	}
 
 	idx := New()
-	it, err := idx.Execute(root, "//uast:Identifier[Name='Foo']")
+	it, err := idx.Execute(root, "//uast:Identifier/Name[text() = 'Foo']/..")
 	require.NoError(t, err)
 	expect(t, it, root[0])
 
 	it, err = idx.Execute(root, "//Identifier")
 	require.NoError(t, err)
 	expect(t, it)
+}
+
+func TestFilterObject(t *testing.T) {
+	b := nodes.Object{
+		uast.KeyType: nodes.String("B"),
+	}
+	c := nodes.Object{
+		uast.KeyType: nodes.String("C"),
+	}
+	d := nodes.Object{
+		uast.KeyType: nodes.String("d:X"),
+	}
+	v := nodes.String("val")
+	va, vb := nodes.String("a"), nodes.String("b")
+	varr := nodes.Array{
+		va,
+		vb,
+	}
+	var root = nodes.Object{
+		uast.KeyType: nodes.String("A"),
+		"key":        v,
+		"keys":       varr,
+		"one":        b,
+		"sub": nodes.Array{
+			c,
+			d,
+		},
+	}
+	/*
+		<A>
+			<key>val</key>
+			<keys>a</keys>
+			<keys>b</keys>
+			<one>
+				<B></B>
+			</one>
+			<sub>
+				<C></C>
+			</sub>
+		</A>
+	*/
+
+	idx := New()
+
+	queries := []struct {
+		name string
+		qu   string
+		exp  []nodes.Node
+	}{
+		{
+			name: "root", qu: "/",
+			exp: []nodes.Node{root},
+		},
+		{
+			name: "root tag", qu: "/A",
+			exp: []nodes.Node{root},
+		},
+		{
+			name: "field obj", qu: "/A/one",
+			exp: []nodes.Node{b},
+		},
+		{
+			name: "field obj tag", qu: "/A/one/B",
+			exp: []nodes.Node{b},
+		},
+		{
+			name: "field obj arr", qu: "/A/sub",
+			exp: []nodes.Node{nodes.Array{c, d}},
+		},
+		{
+			name: "field obj arr tag", qu: "/A/sub/C",
+			exp: []nodes.Node{c},
+		},
+		{
+			name: "inner field", qu: "//one",
+			exp: []nodes.Node{b},
+		},
+		{
+			name: "inner obj", qu: "//B",
+			exp: []nodes.Node{b},
+		},
+		{
+			name: "inner obj 2", qu: "//C",
+			exp: []nodes.Node{c},
+		},
+		{
+			name: "inner obj ns", qu: "//d:X",
+			exp: []nodes.Node{d},
+		},
+		{
+			name: "field value", qu: "/A/key",
+			exp: []nodes.Node{v},
+		},
+		{
+			name: "field value text", qu: "/A/key[text() = 'val']",
+			exp: []nodes.Node{v},
+		},
+		{
+			name: "field value arr", qu: "/A/keys",
+			exp: []nodes.Node{varr},
+		},
+		{
+			name: "text", qu: "//*[text() = 'a']",
+			exp: []nodes.Node{varr},
+		},
+		// TODO: fix in xpath library
+		//{
+		//	name: "field value arr elem", qu: "/A/keys/",
+		//	exp: []nodes.Node{va, vb},
+		//},
+	}
+
+	for _, c := range queries {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			it, err := idx.Execute(root, c.qu)
+			require.NoError(t, err)
+			expect(t, it, c.exp...)
+		})
+	}
 }
 
 func expect(t testing.TB, it query.Iterator, exp ...nodes.Node) {
