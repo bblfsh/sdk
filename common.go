1e9d7229700bf53c9583305a4c51b87ee6ab18d3
Alexander Bezzubov <bzz@apache.org>
2018-11-22 13:28:48 +0100

    refactoring of GRPCOptions()
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- common.go
+++ common.go
@@ -15,29 +15,28 @@ const (
 	// MaxMsgSizeCLIDesc is description for the CLI flag to set max msg size.
 	MaxMsgSizeCLIDesc = "max. message size to send/receive to/from clients (in MB)"
 
-	// DefaulGRPCMaxMsgSizeMb is maximum msg size for gRPC in Mb.
-	DefaulGRPCMaxMsgSizeMb = 100
-	gRPCMaxMsgSizeCapMb    = 2048
+	// DefaulGRPCMaxSendRecvMsgSizeMB is maximum msg size for gRPC in MB.
+	DefaulGRPCMaxSendRecvMsgSizeMB = 100
+	maxMsgSizeCapMB                = 2048
 )
 
-// GRPCOptions returns a slice of gRPC server options with the maximum
-// message size set for sending and receiving.
-// Is intended to be shared by gRPC in bblfshd Server and Drivers.
-// Sets the hard limit of message size to less than 2GB since
-// it may overflow an int value, and it should be big enough.
-func GRPCOptions(maxMessageSizeMb int) ([]grpc.ServerOption, error) {
+// GRPCOptions returns a slice of gRPC server options with the max
+// message size the server can send/receive set.
+// If a >2GB value is requested: the maximum size limit is capped
+// at 100 MB and an error is returned.
+// It is intended to be shared by gRPC in bblfshd Server and Drivers.
+func GRPCOptions(maxMessageSizeMB int) ([]grpc.ServerOption, error) {
 	var err error
-	size := maxMessageSizeMb
-	if size >= gRPCMaxMsgSizeCapMb {
+	sizeMB := maxMessageSizeMB
+	if sizeMB >= maxMsgSizeCapMB {
 		err = fmt.Errorf("%s=%d is too big (limit is %dMB), using %d instead",
-			MaxMsgSizeCLIName, size, gRPCMaxMsgSizeCapMb-1, DefaulGRPCMaxMsgSizeMb)
-		size = DefaulGRPCMaxMsgSizeMb
+			MaxMsgSizeCLIName, sizeMB, maxMsgSizeCapMB-1, DefaulGRPCMaxSendRecvMsgSizeMB)
+		sizeMB = DefaulGRPCMaxSendRecvMsgSizeMB
 	}
 
-	size = size * 1024 * 1024
-
+	sizeBytes := sizeMB * 1024 * 1024
 	return []grpc.ServerOption{
-		grpc.MaxRecvMsgSize(size),
-		grpc.MaxSendMsgSize(size),
+		grpc.MaxRecvMsgSize(sizeBytes),
+		grpc.MaxSendMsgSize(sizeBytes),
 	}, err
 }
