36c1ee565357d0d0c64add5d426e64ef459e77bf
Denys Smirnov <denys@sourced.tech>
2019-05-30 18:44:06 +0300

    positioner: add reverse conversions from UTF-8/16 offset to byte offset
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- uast/transformer/positioner/positions.go
+++ uast/transformer/positioner/positions.go
@@ -302,6 +302,30 @@ func (idx *Index) RuneOffset(offset int) (int, error) {
 	return s.byteOff + s.runeSize8*(offset-s.firstRuneInd), nil
 }
 
+// ToRuneOffset returns a zero-based Unicode character offset given a zero-based byte offset.
+func (idx *Index) ToRuneOffset(offset int) (int, error) {
+	if idx.spans == nil {
+		return 0, errors.New("unicode index is disabled")
+	}
+	last := idx.size
+	if offset < 0 || offset > last {
+		return -1, fmt.Errorf("byte offset out of bounds: %d [%d, %d)", offset, 0, last)
+	} else if offset == last {
+		// special case — EOF position
+		if len(idx.spans) == 0 {
+			return 0, nil
+		}
+		s := idx.spans[len(idx.spans)-1]
+		return s.firstRuneInd + s.numRunes, nil
+	}
+	i := sort.Search(len(idx.spans), func(i int) bool {
+		s := idx.spans[i]
+		return offset < s.byteOff
+	})
+	s := idx.spans[i-1]
+	return s.firstRuneInd + (offset-s.byteOff)/s.runeSize8, nil
+}
+
 // UTF16Offset returns a zero-based byte offset given a zero-based UTF-16 code point offset.
 func (idx *Index) UTF16Offset(offset int) (int, error) {
 	if idx.spans == nil {
@@ -324,4 +348,28 @@ func (idx *Index) UTF16Offset(offset int) (int, error) {
 	})
 	s := idx.spans[i-1]
 	return s.byteOff + s.runeSize8*((offset-s.firstUTF16Ind)/s.runeSize16), nil
+}
+
+// ToUTF16Offset returns a zero-based UTF-16 code point offset given a zero-based byte offset.
+func (idx *Index) ToUTF16Offset(offset int) (int, error) {
+	if idx.spans == nil {
+		return 0, errors.New("unicode index is disabled")
+	}
+	last := idx.size
+	if offset < 0 || offset > last {
+		return -1, fmt.Errorf("byte offset out of bounds: %d [%d, %d)", offset, 0, last)
+	} else if offset == last {
+		// special case — EOF position
+		if len(idx.spans) == 0 {
+			return 0, nil
+		}
+		s := idx.spans[len(idx.spans)-1]
+		return s.firstUTF16Ind + s.numRunes*s.runeSize16, nil
+	}
+	i := sort.Search(len(idx.spans), func(i int) bool {
+		s := idx.spans[i]
+		return offset < s.byteOff
+	})
+	s := idx.spans[i-1]
+	return s.firstUTF16Ind + (offset-s.byteOff)/s.runeSize16, nil
 }
